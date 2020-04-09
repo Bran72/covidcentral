12f8ca80383f7e8f1e5acb28411678f40c2ec788
Brandon Leininger <brandon.leininger@icloud.com>
2020-04-09 19:00:36 +0200

    Starting global integration: styling, rrouting,...

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+
+    /**
+     * @Route("/depistage", name="depistage")
+     */
+    public function depistage()
+    {
+        $user = $this->getUser();
+        return $this->render('default/depistage.html.twig', [
+            'user' => $user
+        ]);
+    }
+}
