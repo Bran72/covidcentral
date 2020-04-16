04b6e2cf3a1d00d5f7d832be2383f68ed871e785
robingiraud <robin.giraud5@gmail.com>
2020-04-16 18:50:29 +0200

    :sparkles: Update form Depistage + Migrations

--- src/Form/DepistageType.php
+++ src/Form/DepistageType.php
@@ -7,26 +7,63 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class DepistageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('q1', NumberType::class, [
-                'html5'=>true
+            ->add('q1', RangeType::class, [
+                'attr'=>[
+                    'min' => 0,
+                    'max' => 10
+                ]
             ])
-            ->add('q2')
-            ->add('q3')
+            ->add('q2', ChoiceType::class,[
+                'choices' =>  [
+                    'Oui' => true,
+                    'Non' => false],
+                'multiple'=>false,
+                'expanded'=>true,
+            ])
+            ->add('q3', ChoiceType::class,[
+                'choices' =>  [
+                    'Oui' => true,
+                    'Non' => false],
+                'multiple'=>false,
+                'expanded'=>true,
+            ])
             ->add('q4', NumberType::class, [
                 'html5'=>true
             ])
-            ->add('q5')
-            ->add('q6')
+            ->add('q5', ChoiceType::class,[
+                'choices' =>  [
+                    'Oui' => true,
+                    'Non' => false],
+                'multiple'=>false,
+                'expanded'=>true,
+            ])
+            ->add('q6', ChoiceType::class,[
+                'choices' =>  [
+                    'Oui' => true,
+                    'Non' => false],
+                'multiple'=>false,
+                'expanded'=>true,
+            ])
             ->add('q7', NumberType::class, [
                 'html5'=>true
             ])
-            ->add('q8')
+            ->add('q8', ChoiceType::class,[
+                'choices' =>  [
+                    'Oui' => true,
+                    'Non' => false],
+                'multiple'=>false,
+                'expanded'=>true,
+            ])
+            ->add('send', SubmitType::class)
         ;
     }
 
