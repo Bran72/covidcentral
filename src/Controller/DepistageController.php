11ecc369683b6640e6d0db8a3f7165b47bc796f4
robingiraud <robin.giraud5@gmail.com>
2020-04-23 16:08:22 +0200

    :art: Adding CSS to depistage form

--- src/Controller/DepistageController.php
+++ src/Controller/DepistageController.php
@@ -5,10 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\QuestionDepistage;
 use App\Form\DepistageType;
-use Symfony\Component\Security\Core\Security;
 
 class DepistageController extends AbstractController
 {
@@ -25,15 +23,30 @@ class DepistageController extends AbstractController
 
         $form -> handleRequest($request);
 
-        if($form -> isSubmitted() && $form -> isValid()){
-            if ($this->getUser()) {
-                $user = $this->getUser();
-                $question->setUser($user);
-                $entityManager -> persist($question);
-                $entityManager -> flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
 
-                return $this->render('depistage/rendezvous.html.twig');
-            }
+            $question->setUser($user);
+            $entityManager -> persist($question);
+            $entityManager -> flush();
+
+            $points = 0;
+            // var_dump($form->get('q1')->getData());
+            $points += intval($form->get('q1')->getData())*3;
+            $points += intval($form->get('q2')->getData())*10;
+            $points += intval($form->get('q3')->getData())*10;
+            if (intval($form->get('q4')->getData()) > 38)
+                $points += 10;
+            $points += intval($form->get('q5')->getData())*10;
+            $points += intval($form->get('q6')->getData())*10;
+            if (intval($form->get('q4')->getData()) > 110)
+                $points += 10;
+            $points += intval($form->get('q8')->getData())*10;
+
+
+            return $this->render('depistage/rendezvous.html.twig', [
+                'points' => $points
+            ]);
         }
 
         return $this->render('depistage/index.html.twig', [
